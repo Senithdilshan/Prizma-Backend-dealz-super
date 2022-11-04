809eb6ef121730893a0020901684e6ee33ddb1f8
anojfernando <anojfern@gmail.com>
2022-11-04 13:25:48 +0530

    customer search and csv file added

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import CustomerRouter from './routers/Customer/manage-customers.router';
 const app = express()
 app.use(cors())
 app.use(express.json());
+app.use(express.static('uploads'));
 const prisma =new PrismaClient();
 
 app.use('/product', ProductRouter)
