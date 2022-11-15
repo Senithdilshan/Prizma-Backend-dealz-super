9dcda5093960d5cbd97e63327c7ae7d51eb10dfe
AkilaJava <akilajava99@gmail.com>
2022-11-16 02:02:56 +0530

    bank and supplier payment

---
+++ src/routers/Supplier/supplierpayment.router.ts
@@ -0,0 +1,40 @@
+import { Request, Response, Router } from 'express';
+import { PrismaClient } from "@prisma/client";
+
+const router = Router()
+const prisma = new PrismaClient();
+
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        const { paymentId , supplierId , supplierName , paymentDescription, paymentAmount } = req.body;
+        console.log(req.body);
+
+        const supplier_payment = await prisma.supplier_payments.create({
+            data: {
+                paymentId : paymentId ,
+                supplierId : supplierId ,
+                supplierName : supplierName ,
+                paymentDescription : paymentDescription ,
+                paymentAmount : paymentAmount,
+            },
+        });
+        res.send(supplier_payment);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
+});
+
+router.get("/", async (req: Request, res: Response) => {
+    try {
+        const supplierPayments = await prisma.supplier_payments.findMany(
+        );
+        res.send(supplierPayments);
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
+});
+
+export default router ;
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import BatchRouter from './routers/Stock/batch.router'
 import StockRouter from './routers/Stock/stock.router'
 import CustomerRouter from './routers/Customer/manage-customers.router';
 import SupplierRouter from './routers/Supplier/supplier.router' ;
+import BankRouter from './routers/Bank/bank.router' ;
+import SupplierPaymentRouter from './routers/Supplier/supplierpayment.router' ;
 
 const app = express()
 app.use(cors())
@@ -20,6 +22,8 @@ app.use('/batches', BatchRouter)
 app.use('/stock',StockRouter)
 app.use('/manage-customers',CustomerRouter)
 app.use('/supplier', SupplierRouter)
+app.use('/bank' , BankRouter)
+app.use('/supplier_payments' , SupplierPaymentRouter)
 
 app.listen(5000, () => {
     console.log("server running on port 5000")
